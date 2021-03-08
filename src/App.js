d5069762f7a2fdb3afbb0feb68ff3b11502eb594
shivaram549 <shivarampandramish549@gmail.com>
2021-03-08 13:34:29 +0530

    basic product rendering

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Products from "./components/Products/Products";
+
+const App = () => {
+  return (
+    <div>
+      E-Commerce
+      <Products />
+    </div>
+  );
+};
+
+export default App;
